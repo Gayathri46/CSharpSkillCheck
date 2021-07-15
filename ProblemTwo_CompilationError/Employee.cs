802caff87a1675c474da00be377340501e97a93e
Gayathri46 <87111467+Gayathri46@users.noreply.github.com>
2021-07-15 17:39:16 +0530

    Update Employee.cs

--- ProblemTwo_CompilationError/Employee.cs
+++ ProblemTwo_CompilationError/Employee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +6,25 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class Employee
+    public class Employee
     {
         public int EmpID { get; set; }
         public string EmpName { get; set; }
         public int EmpLevel { get; set; }
-        private int NoOfLeaves { get; }
+        public int NoOfLeaves { get; set; }
         public DateTime DateOfJoining { get; set; }
 
         public Employee()
         {
             //Default number of leaves credited is 2 per month based on date of joining to the current date
-            NoOfLeaves= GetMonthDifference(DateOfJoining, DateTime.Now) * 2;
+            NoOfLeaves = GetMonthDifference(DateOfJoining, DateTime.Now) * 2;
         }
-        public Employee(int eID,string eName,int eLevel, DateTime eDoJ)
+        public Employee(int eID, string eName, int eLevel, DateTime eDoJ)
         {
-
+            EmpID = eID;
+            EmpName = eName;
+            EmpLevel = eLevel;
+            DateOfJoining = eDoJ;
         }
 
         public static int GetMonthDifference(DateTime startDate, DateTime endDate)
